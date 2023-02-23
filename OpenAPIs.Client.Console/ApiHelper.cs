09ea54c53cfe6dd04cbc79d2897c9dba787b988a
Chet Gray <chet.gray@waystar.com>
2023-02-23 15:43:53 -0500

    ✨ feat: implement `ApiHelper` to set up an `HttpClient`

---
+++ OpenAPIs.Client.Console/ApiHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace OpenAPIs.Client.Console
+{
+    /// <summary>Provides utilities for use by the API BL classes.</summary>
+    internal static class ApiHelper
+    {
+        /// <summary>
+        /// Gets or sets the static <see cref="HttpClient">HTTP client</see> used by the API BL
+        /// classes.
+        /// </summary>
+        public static HttpClient ApiClient { get; set; } = new HttpClient();
+
+        /// <summary>
+        /// Initializes the <see cref="ApiClient">HTTP client</see> to accept JSON responses.
+        /// </summary>
+        public static void InitializeClient()
+        {
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+            );
+        }
+    }
+}
