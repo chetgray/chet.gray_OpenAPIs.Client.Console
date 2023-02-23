2bfb69dfc4689786954ab79f358444ee122f02f9
Chet Gray <chet.gray@waystar.com>
2023-02-23 17:12:07 -0500

    ✨ feat: implement menu loop
    > - [x] The Console App should have a menu that lets the user choose
    >   which of the 5 APIs to call
    > - [x] After it's done, it should ask the user if they want to do it
    >   again.
    > - [x] If they answer Yes, it should present the Main Menu again.

--- OpenAPIs.Client.Console/Program.cs
+++ OpenAPIs.Client.Console/Program.cs
@@ -1,10 +1,65 @@
-﻿namespace OpenAPIs.Client.Console
+﻿using static System.Console;
+
+namespace OpenAPIs.Client.Console
 {
     internal static class Program
     {
         private static void Main()
         {
             ApiHelper.InitializeClient();
+
+            bool shouldContinue = true;
+            while (shouldContinue)
+            {
+                WriteLine(
+                    "[1] Lookup place by post code\n"
+                        + "[2] Look up post codes by place\n"
+                        + "[3] \n"
+                        + "[4] \n"
+                        + "[5] \n"
+                        + "[0] Exit\n"
+                );
+                Write("Which API would you like to call?\n» ");
+                string menuInput = ReadLine();
+                switch (menuInput)
+                {
+                    case "1":
+                        break;
+
+                    case "2":
+                        break;
+
+                    case "3":
+                        break;
+
+                    case "4":
+                        break;
+
+                    case "5":
+                        break;
+
+                    case "0":
+                        shouldContinue = false;
+                        break;
+
+                    default:
+                        WriteLine("Invalid choice");
+                        continue;
+                }
+                if (!shouldContinue)
+                {
+                    break;
+                }
+                WriteLine("Would you like to make another choice? ([y]/n)\n» ");
+                string continueInput = ReadLine();
+                if (continueInput.StartsWith("n", ignoreCase: true, culture: null))
+                {
+                    shouldContinue = false;
+                }
+            }
+            WriteLine("\nGoodbye!\n");
+            WriteLine("Press any key to exit . . . ");
+            ReadKey(intercept: true);
         }
     }
 }
